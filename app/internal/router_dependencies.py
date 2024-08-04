121d8d8cc7bac48fe23ccbdb144aca9e2815e15e
Luis Delgado <luisdelgado@Luiss-Air.attlocal.net>
2024-08-03 23:38:06 -0400

    Got Deepgram working with new layer of indirection

---
+++ app/internal/router_dependencies.py
@@ -0,0 +1,18 @@
+from ..dependencies.api.deepgram_base_class import DeepgramBaseClass
+from ..dependencies.api.docupanda_base_class import DocupandaBaseClass
+from ..dependencies.api.openai_base_class import OpenAIBaseClass
+from ..dependencies.api.pinecone_base_class import PineconeBaseClass
+from ..dependencies.api.supabase_factory_base_class import SupabaseFactoryBaseClass
+
+class RouterDependencies:
+    def __init__(self,
+                 openai_client: OpenAIBaseClass = None,
+                 pinecone_client: PineconeBaseClass = None,
+                 deepgram_client: DeepgramBaseClass = None,
+                 docupanda_client: DocupandaBaseClass = None,
+                 supabase_client_factory: SupabaseFactoryBaseClass = None):
+        self.openai_client = openai_client
+        self.pinecone_client = pinecone_client
+        self.docupanda_client = docupanda_client
+        self.deepgram_client = deepgram_client
+        self.supabase_client_factory = supabase_client_factory
---
+++ app/internal/schemas.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class Gender(Enum):
+    UNDEFINED = "undefined"
+    MALE = "male"
+    FEMALE = "female"
+    OTHER = "other"
+    RATHER_NOT_SAY = "rather_not_say"
